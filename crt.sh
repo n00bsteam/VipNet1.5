f4f71a90664094dc8549f53470d16c53554ddb06
n0bsteam <pakhmov.dan@gmail.com>
2021-02-26 15:33:40 +0300

    work

--- crt.sh
+++ crt.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#Скрипт проверки подключенного контейнера
-#номер контейнера:
-#regexp: "^[a-z]{3}-[a-z]-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}$"
-#rnd-c-1a81-a680-6909-261d-33bb-5a1b-2cce
-#rnd-8-a5e5-b9e6-a990-5962-36d6-4c95-aa00
-status=false
+# Скрипт добавления сертификата из контейнера 
+# в личное хранилище пользователя
+# с привязкой к контейнеру
 
+#переменные
+status=false
+p=~/
+
+# Функция преобразования информации контейнера
 function container_check {
 	#проверяем подключенные контейнеры
 	a=$(/opt/itcs/bin/csp-gost print_containers)
@@ -19,22 +21,25 @@ while [[ $status == "false" ]]
 	do
 	container_check
 	#сверяем по ключевой фразе в массиве
-	if [[ "${a[1]}" == "<nocontainersavailable>" ]]
-		then
-		echo "NO CONTAINER"
-		else
-		echo "CONTAINER CONNECTED"
+	if [[ "${a[1]}" == "<nocontainersavailable>" ]] 
+		then 
+		echo "Контейнер не найдет, подключите контейнер."
+		else 
+		echo "Контейнер найден, экспортируем сертификат..."
 		status=true
 		a=${a[1]}
 		a=(${a//:/ })
-		#echo "${a[1]}"
 		break
 	fi
 done
-echo "${a[1]}"
-echo `/opt/itcs/bin/csp-gost container_content --container "${a[1]}"`
-#if [[ $status == "true" ]]
-#	then
-#	container_check
-#	echo ${a[1]}
-#fi
+
+#echo "${a[1]}"
+#echo `/opt/itcs/bin/csp-gost container_content --container "${a[1]}"`
+#echo `/opt/itcs/bin/certmgr print_certificate --container "${a[1]}"`
+
+/opt/itcs/bin/certmgr extract_certificate --file="$p/crt.cer" --container="${a[1]}"
+echo "Импортируем сертификат в личное хранилище..."
+echo "Связываем сертификат с контейнером..."
+/opt/itcs/bin/certmgr add_certificate --location=CurrentUser --store=My --file="$p/crt.cer" --container="${a[1]}"
+echo "Сертификат установлен, удачного пользования ;)"
+
