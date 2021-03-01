96907c32b62c79c1b58a0146ab683ec634687ac9
n0bsteam <pakhmov.dan@gmail.com>
2021-03-01 15:37:19 +0300

    some tune

--- crt.sh
+++ crt.sh
@@ -1,45 +1,23 @@
 #!/bin/bash
-# Скрипт добавления сертификата из контейнера 
+# Скрипт добавления сертификата из контейнера
 # в личное хранилище пользователя
 # с привязкой к контейнеру
 
-#переменные
-status=false
-p=~/
-
-# Функция преобразования информации контейнера
-function container_check {
-	#проверяем подключенные контейнеры
-	a=$(/opt/itcs/bin/csp-gost print_containers)
-	#убираем пробелы
-	a=${a// /}
-	#режем на массив
-	a=(${a// / })
-}
-
-while [[ $status == "false" ]]
-	do
-	container_check
-	#сверяем по ключевой фразе в массиве
-	if [[ "${a[1]}" == "<nocontainersavailable>" ]] 
-		then 
-		echo "Контейнер не найдет, подключите контейнер."
-		else 
-		echo "Контейнер найден, экспортируем сертификат..."
-		status=true
-		a=${a[1]}
-		a=(${a//:/ })
-		break
-	fi
-done
-
-#echo "${a[1]}"
-#echo `/opt/itcs/bin/csp-gost container_content --container "${a[1]}"`
-#echo `/opt/itcs/bin/certmgr print_certificate --container "${a[1]}"`
-
-/opt/itcs/bin/certmgr extract_certificate --file="$p/crt.cer" --container="${a[1]}"
-echo "Импортируем сертификат в личное хранилище..."
-echo "Связываем сертификат с контейнером..."
-/opt/itcs/bin/certmgr add_certificate --location=CurrentUser --store=My --file="$p/crt.cer" --container="${a[1]}"
-echo "Сертификат установлен, удачного пользования ;)"
-
+#запрашиваем контейнеры
+a=$(/opt/itcs/bin/csp-gost print_containers)
+#фильтруем названия контейнеров
+temp=`echo "$a" | grep -o --text '^  [0-9].*$' | sed -r '/^  (.*)\): (.*)$/ s//\2/'`
+#проверяем, есть ли контейнеры
+if [ -z "$temp" ];
+then
+                echo "нет контейнеров"
+else
+                #режем на массив
+                IFS=$'\n' arr=($temp)
+                #перебираем массив
+                for i in ${arr[*]};do
+                               echo "устанавливаем контейнер $i"
+                               /opt/itcs/bin/certmgr add_certificate --location=CurrentUser --store=My --container="$i"
+                done
+fi
+echo "Готово"
