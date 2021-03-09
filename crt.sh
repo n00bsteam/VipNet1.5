7267b2dba481495e64a4c5f6705e1ebb9ce12b31
n0bsteam <pakhmov.dan@gmail.com>
2021-03-09 10:43:43 +0300

    log issue fix

--- crt.sh
+++ crt.sh
@@ -1,27 +1,23 @@
-#!/bin/sh -x
-exec >>/var/log/crt.log 2>&1
-date
-env
+#!/bin/bash -efux
+exec >/var/log/crt.log 2>&1
+/bin/date
+/usr/bin/env
 
 # Скрипт добавления сертификата из контейнера
 # в личное хранилище пользователя
 # с привязкой к контейнеру
 
 #запрашиваем контейнеры
-a=$(/opt/itcs/bin/csp-gost print_containers)
-#фильтруем названия контейнеров
-temp=`echo "$a" | grep -o --text '^  [0-9].*$' | sed -r '/^  (.*)\): (.*)$/ s//\2/'`
-#проверяем, есть ли контейнеры
-if [ -z "$temp" ];
-then
-                echo "нет контейнеров"
+
+dummy="$(/opt/itcs/bin/csp-gost print_containers |
+        grep -o --text '^  [0-9].*$' |
+        sed -r '/^  (.*)\): (.*)$/ s//\2/')"
+if [ -z "$dummy" ]; then
+        echo "нет контейнеров"
 else
-                #режем на массив
-                IFS=$'\n' arr=($temp)
-                #перебираем массив
-                for i in ${arr[*]};do
-                               echo "устанавливаем контейнер $i"
-                               /opt/itcs/bin/certmgr add_certificate --location=CurrentUser --store=My --container="$i"
-                done
+        for i in $dummy; do
+                echo "устанавливаем контейнер $i"
+                /opt/itcs/bin/certmgr add_certificate --location=CurrentUser --store=My --container="$i"
+        done
 fi
 echo "Готово"
